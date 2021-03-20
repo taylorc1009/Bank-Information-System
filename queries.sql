736cad30ea091451df3c189170efdadf3a06bb0b
Taylor Courtney <taylorcourtney2000@googlemail.com>
2021-03-20 14:16:29 +0000

    Fixed query 'b' and added failed query 'c' attempts

--- queries.sql
+++ queries.sql
@@ -6,22 +6,48 @@ where instr(lower(p.pName.firstName), 'st') != 0 and p.addr.city = 'Glasgow' and
     from EmployeeTable emp
 );
 
+
+
+
+
 /* query 'b' */
 select count(distinct deref(acnt.bID).bID) as accounts_at_branch, br.getAddress() as address
 from AccountTable acnt
 join BranchTable br on (deref(acnt.bID).bID = br.bID)
 group by br.bID, br.getAddress();
 
-/* query 'c' */
-select br.getAddress(),
-    acnt.getCustomerNames() as customers,
-    acnt.balance as account_balance
-from AccountTable acnt
-join BranchTable br on (deref(acnt.bID).bID = br.bID)
+
+
+
+
+/* query 'c' - none of these solutions work: the first 2 don't limit to 1 account per branch and the third doesn't execute due to an unexplained error */
+select br.getAddress() as address, acnt.getCustomerNames() as customers, max(acnt.balance) as balance
+from BranchTable br
+join AccountTable acnt on (br.bID = deref(acnt.bID).bID)
+where acnt.accType = 'savings'
+group by br.bID, br.getAddress(), acnt.getCustomerNames(), acnt.balance
+order by acnt.balance desc;
+
+select br.getAddress() as address, acnt.getCustomerNames() as customers, acnt.balance as balance
+from BranchTable br
+join AccountTable acnt on (br.bID = deref(acnt.bID).bID)
+where acnt.accType = 'savings' and acnt.accNum in (
+    select first_value(sAcnt.accNum) ignore nulls over (partition by sAcnt.accNum order by acnt.balance rows between unbounded preceding and unbounded following)
+    from AccountTable sAcnt
+    group by sAcnt.accNum, deref(sAcnt.bID).bID
+)
+group by br.bID, br.getAddress(), acnt.getCustomerNames(), acnt.balance
+order by acnt.balance desc;
+
+select br.getAddress() as address, acnt.getCustomerNames() as customers, acnt.balance as balance
+from BranchTable br
+join AccountTable acnt on (br.bID = deref(acnt.bID).bID)
 where acnt.accType = 'savings' and acnt.accNum in (
     select sAcnt.accNum
     from AccountTable sAcnt
-    group by sAcnt.accNum, deref(sAcnt.bID).bID, sAcnt.balance
-    order by sAcnt.balance
+    group by sAcnt.accNum, deref(sAcnt.bID).bID
+    order by sAcnt.balance desc
+    fetch first row only
 )
-group by br.bID, br.getAddress(), acnt.getCustomerNames(), acnt.balance;
+group by br.bID, br.getAddress(), acnt.getCustomerNames(), acnt.balance
+order by acnt.balance desc;
