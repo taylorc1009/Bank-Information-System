1e0e760b4cf8794305e04272672e02900d6611b4
Taylor Courtney <taylorcourtney2000@googlemail.com>
2021-03-20 10:10:39 +0000

    Finished query 'b'

--- queries.sql
+++ queries.sql
@@ -1,7 +1,17 @@
 /* query 'a' */
-select p.pName.firstName, p.pName.surName, p.addr.city
+select p.pName.firstName as forename, p.pName.surName as surname
 from PersonTable p
 where instr(lower(p.pName.firstName), 'st') != 0 and p.addr.city = 'Glasgow' and p.persID in (
     select deref(emp.pers).persID
     from EmployeeTable emp
 );
+
+/* query 'b' */
+select count(distinct deref(acnt.bID).bID) as accounts_at_branch,
+    br.addr.buildingNum as building_number,
+    br.addr.street as street,
+    br.addr.city as city,
+    br.addr.postCode as post_ode
+from AccountTable acnt
+join BranchTable br on (deref(acnt.bID).bID = br.bID)
+group by br.bID, br.addr.buildingNum, br.addr.street, br.addr.city, br.addr.postCode;
