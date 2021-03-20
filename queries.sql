a9d0d044a56b6976767f8ac60e5d8e33f6a987ad
Taylor Courtney <taylorcourtney2000@googlemail.com>
2021-03-20 11:10:09 +0000

    Finished query 'c'

--- queries.sql
+++ queries.sql
@@ -11,7 +11,21 @@ select count(distinct deref(acnt.bID).bID) as accounts_at_branch,
     br.addr.buildingNum as building_number,
     br.addr.street as street,
     br.addr.city as city,
-    br.addr.postCode as post_ode
+    br.addr.postCode as post_code
 from AccountTable acnt
 join BranchTable br on (deref(acnt.bID).bID = br.bID)
 group by br.bID, br.addr.buildingNum, br.addr.street, br.addr.city, br.addr.postCode;
+
+/* query 'c' */
+select br.addr.buildingNum as building_number,
+    br.addr.street as street,
+    br.addr.city as city,
+    br.addr.postCode as post_code,
+    acnt.getCustomerNames() as customers,
+    acnt.balance as account_balance
+from AccountTable acnt
+join BranchTable br on (deref(acnt.bID).bID = br.bID)
+where acnt.accType = 'savings'
+group by br.bID, br.addr.buildingNum, br.addr.street, br.addr.city, br.addr.postCode, 
+acnt.getCustomerNames(), acnt.balance
+order by acnt.balance;
